376458aa9a44f6f650cddd33043640d2fb1463b5
Salih Candir <salih@live.at>
2023-02-11 12:28:49 +0100

    feat: use username from url parameter

--- src/user/infrastructure/controller/add_user_handler.rs
+++ src/user/infrastructure/controller/add_user_handler.rs
@@ -1,11 +1,17 @@
-use actix_web::Responder;
+use actix_web::{Responder, web};
+use serde::Deserialize;
 
 use crate::user::core::use_case::add_user::{AddUserCommand, AddUserUseCase};
 use crate::user::core::use_case::add_user_interceptor::AddUserInterceptor;
 use crate::user::infrastructure::in_memory_user_repository::InMemoryUserRepository;
 use crate::user::infrastructure::user_http_response_presenter::UserHttpResponsePresenter;
 
-pub async fn add_user_handler() -> impl Responder {
+#[derive(Deserialize)]
+pub struct AddUserParameter {
+    username: String,
+}
+
+pub async fn add_user_handler(url_param: web::Path<AddUserParameter>) -> impl Responder {
     let user_exists_repository = InMemoryUserRepository::new();
     let mut add_user_repository = InMemoryUserRepository::new();
     let mut add_user_use_case = AddUserInterceptor::new(
@@ -13,7 +19,7 @@ pub async fn add_user_handler() -> impl Responder {
         &user_exists_repository,
     );
     let command = AddUserCommand::new(
-        String::from("csalih"),
+        String::from(&url_param.username),
         String::from("new"),
     );
     let mut presenter = UserHttpResponsePresenter::new();
--- src/user/infrastructure/controller/user_controller.rs
+++ src/user/infrastructure/controller/user_controller.rs
@@ -4,6 +4,6 @@ use crate::user::infrastructure::controller::add_user_handler::add_user_handler;
 
 pub fn add_routes(config: &mut web::ServiceConfig) {
     config
-        .route("/api/v1/users", web::get().to(add_user_handler));
+        .route("/api/v1/users/{username}", web::post().to(add_user_handler));
 }
 
